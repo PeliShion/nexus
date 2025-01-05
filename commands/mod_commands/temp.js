4dd477ae5b5214be327d9ae07db2189c33cff21b
PeliShion <while_ago@outlook.com>
2025-01-05 17:32:38 +0800

    using .find instead of indexing for searches

--- commands/mod_commands/temp.js
+++ commands/mod_commands/temp.js
@@ -9,10 +9,10 @@ module.exports = {
 
     async execute(interaction) {
         if (interaction.user.id !== "492965189038374933") return await interaction.reply({ content: redtext("You need to be a moderator to use this command!"), ephemeral: true })
-        for (let i = 600; i < listofauctions.length + 1; i++) {
-            let ahcheck = listofauctions[i]
+        for (let i = 1100; i < listofauctions.length + 1; i++) {
+            let ahcheck = listofauctions.find(x => x.id === id)
             if(!ahcheck) continue
-            console.log(i)
+            console.log(ahcheck.id)
             if(ahcheck.active === true) {
                 ahcheck.endtime = ahcheck.endtime + 84000
             }

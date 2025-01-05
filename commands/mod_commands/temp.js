5951cbb25c846b55a4b9e0935178edd1d83a6ede
PeliShion <while_ago@outlook.com>
2025-01-05 17:25:26 +0800

    deploying an untested code was a bad idea

--- commands/mod_commands/temp.js
+++ commands/mod_commands/temp.js
@@ -11,7 +11,7 @@ module.exports = {
         if (interaction.user.id !== "492965189038374933") return await interaction.reply({ content: redtext("You need to be a moderator to use this command!"), ephemeral: true })
         for (let i = 1100; i < listofauctions.length + 1; i++) {
             let ahcheck = listofauctions[i]
-            if(!ahcheck) continue
+            if(!ahcheck.id) continue
             console.log(i)
             if(ahcheck.active === true) {
                 ahcheck.endtime = ahcheck.endtime + 84000

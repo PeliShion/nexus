824b71c2cbdb9984ca0633a81937401b2ccc1ea3
PeliShion <while_ago@outlook.com>
2025-01-05 17:21:07 +0800

    a

--- commands/mod_commands/temp.js
+++ commands/mod_commands/temp.js
@@ -9,8 +9,9 @@ module.exports = {
 
     async execute(interaction) {
         if (interaction.user.id !== "492965189038374933") return await interaction.reply({ content: redtext("You need to be a moderator to use this command!"), ephemeral: true })
-        for (let i = 0; i < listofauctions.length + 1; i++) {
+        for (let i = 500; i < listofauctions.length + 1; i++) {
             let ahcheck = listofauctions[i]
+            console.log(i)
             if(ahcheck.active === true) {
                 ahcheck.endtime = ahcheck.endtime + 84000
             }

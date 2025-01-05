acd7a0037cd24b0606ef2b0906da6e5bffb1b752
PeliShion <while_ago@outlook.com>
2025-01-05 17:29:09 +0800

    .

--- commands/mod_commands/temp.js
+++ commands/mod_commands/temp.js
@@ -9,7 +9,7 @@ module.exports = {
 
     async execute(interaction) {
         if (interaction.user.id !== "492965189038374933") return await interaction.reply({ content: redtext("You need to be a moderator to use this command!"), ephemeral: true })
-        for (let i = 1100; i < listofauctions.length + 1; i++) {
+        for (let i = 600; i < listofauctions.length + 1; i++) {
             let ahcheck = listofauctions[i]
             if(!ahcheck.id) continue
             console.log(i)
@@ -17,7 +17,6 @@ module.exports = {
                 ahcheck.endtime = ahcheck.endtime + 84000
             }
             if(i === listofauctions.length) fs.writeFileSync("./data/auctions.json", JSON.stringify(listofauctions, null, 4));
-            continue
         }
     }
 }

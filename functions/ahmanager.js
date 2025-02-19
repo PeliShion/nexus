2c07667fb8e0a4730d76703f477deae706fa23f6
PeliShion <while_ago@outlook.com>
2025-02-19 16:33:02 +0800

    togglenotif now works for 1 hour warning

--- functions/ahmanager.js
+++ functions/ahmanager.js
@@ -471,6 +471,7 @@ module.exports.auccheck = async function () {
                 let aucowner = currentcheck.owner
                 let msguser = notifusers[j]
                 if (msguser === aucowner) continue
+                else if (currentcheck.blocknotif.includes(msguser)) continue
                 client.users.send(msguser, { content: bluetext(`The auction #${auctionid} is going to end in an hour!`) + `\n` + genmessagelink(auctionid), embeds: [module.exports.embedgen(auctionid)], files: [attachment] }).catch((e) => client.channels.fetch(botchannelid).then(channel => channel.send(`<@${msguser}> I tried to message you in DMs, but I couldn't! Please unblock or enable DMs!`)))
             }
         }

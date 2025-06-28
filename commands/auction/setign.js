230415282a11b9442a4d227bfbfc4aceaf52f387
PeliShion <while_ago@outlook.com>
2025-06-28 15:08:00 +0800

    added character limit to setign

--- commands/auction/setign.js
+++ commands/auction/setign.js
@@ -18,6 +18,7 @@ module.exports = {
         if (interaction.channel.id !== botchannelid) return await interaction.reply({ content: redtext("You can only use this bot in charms-discussion!"), ephemeral: true })
         let newign = interaction.options.getString("ign")
         let userdata = alluserdata.find(x => x.userid === interaction.user.id)
+        if(newign.length > 100) return interaction.reply({ content: "The ign has to be less than 100 characters long.", ephemeral: true})
         userdata.ign = newign
         interaction.reply({ content: "Your new ign has been set to: `" + newign + "`", ephemeral: true})
         fs.writeFileSync("./data/userdata.json", JSON.stringify(alluserdata, null, 4))

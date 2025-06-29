4b814ded096b429c94af073155eb6f2952cc5174
PeliShion <while_ago@outlook.com>
2025-06-29 09:46:47 +0800

    fixed an issue where player not having userdata causes an error

--- commands/auction/ign.js
+++ commands/auction/ign.js
@@ -18,12 +18,26 @@ module.exports = {
         let user = interaction.options?.getUser("user")
         if(user) {
             let userdata = alluserdata.find(x => x.userid === user.id)
-            let ign = userdata?.ign;
-            if(!ign) {
-                userdata.ign = "Not Set"
-                interaction.reply({ content: "The user has not set an ign.", ephemeral: true })
+            if(!userdata) {
+                let userdataobject = {
+					"userid": user.id,
+					"auctionswon": 0,
+					"auctionbids": 0,
+					"auctionhosts": 0,
+					"totalharspent": 0,
+					"ign": "Not set"
+				}
+                alluserdata.push(userdataobject)
+                fs.writeFileSync("./data/userdata.json", JSON.stringify(alluserdata, null, 4))
             }
-            else interaction.reply({ content: "Their ign is: `" + ign + "`", ephemeral: true})
+            else {
+                let ign = userdata?.ign;
+                if(!ign) {
+                    userdata.ign = "Not Set"
+                    interaction.reply({ content: "The user has not set an ign.", ephemeral: true })
+                }
+                else interaction.reply({ content: "Their ign is: `" + ign + "`", ephemeral: true})
+            }
         } else {
             let owndata = alluserdata.find(x => x.userid === interaction.user.id)
             let ign = owndata?.ign;
